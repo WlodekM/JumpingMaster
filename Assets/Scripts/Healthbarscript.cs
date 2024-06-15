8af44e74c4fc204cbd1ac2562d115e8969a111de
WlodekM <wlodzio.maly@gmail.com>
2024-06-15 17:33:41 +0300

    loosah

--- Assets/Scripts/Healthbarscript.cs
+++ Assets/Scripts/Healthbarscript.cs
@@ -4,17 +4,12 @@ using UnityEngine;
 
 public class Healthbarscript : MonoBehaviour
 {
-    // add array of heart image objects
-    // make function to update healthbar
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(collision.CompareTag("spike"))
+        {
+            Destroy(gameObject);
+            Debug.LogError("L");
+        }
     }
 }
