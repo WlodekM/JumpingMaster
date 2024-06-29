2d64513b8f919e23ca7d293507cd30b07a515513
WlodekM <wlodzio.maly@gmail.com>
2024-06-29 17:59:13 +0300

    adsasd

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField]
+    GameObject bullet;
+
+    private void Start()
+    {
+        Debug.Log("WHAR!");
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("shoot damn it!");
+        Vector3 lookPos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        Instantiate(bullet, transform.position, Quaternion.LookRotation(lookPos));
+    }
+}
