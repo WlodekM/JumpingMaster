19d55a6fd80490f8c6df8940a4b3d0d67f1ea3e9
WlodekM <wlodzio.maly@gmail.com>
2024-06-29 15:59:33 +0300

    teleport

---
+++ Assets/Scripts/TeleFieldScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleFieldScript : MonoBehaviour
+{
+    GameObject player;
+    plaire playerScript;
+    [SerializeField]
+    GameObject teleportParticle;
+
+    private void Start()
+    {
+        playerScript = GameObject.FindObjectOfType<plaire>();
+        player = playerScript.gameObject;
+    }
+
+    void doTeleport()
+    {
+        player.transform.position = transform.GetChild(0).position;
+        GameObject particle = Instantiate(teleportParticle, player.transform.position, Quaternion.identity);
+        Destroy(particle, 5f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player")) return;
+        Invoke("doTeleport", 1f);
+    }
+}
